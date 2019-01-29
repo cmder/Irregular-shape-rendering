45637411afe1b64d4b9efc3f99627115ad93d00a
Yao Ximing <cmd100000@gmail.com>
2019-01-30 03:19:39 +0800

    add base class

---
+++ CircleRender/app/src/main/java/io/agora/circlerender/activity/CallActivity.java
@@ -0,0 +1,14 @@
+package io.agora.circlerender.activity;
+
+import android.os.Bundle;
+
+import io.agora.circlerender.R;
+
+public class CallActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
+    }
+}
--- CircleRender/app/src/main/java/io/agora/circlerender/activity/MainActivity.java
+++ CircleRender/app/src/main/java/io/agora/circlerender/activity/MainActivity.java
@@ -1,22 +1,13 @@
 package io.agora.circlerender.activity;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
 import android.hardware.Camera;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 
 import io.agora.circlerender.R;
 import io.agora.circlerender.util.MyCameraManager;
 import io.agora.circlerender.widget.CircleRendererView;
-import permissions.dispatcher.NeedsPermission;
-import permissions.dispatcher.RuntimePermissions;
 
-@RuntimePermissions
-public class MainActivity extends Activity {
-    private Context mContext;
-
+public class MainActivity extends BaseActivity {
     private CircleRendererView mCircleRendererView;
     private int mCameraId = Camera.CameraInfo.CAMERA_FACING_FRONT;
     private MyCameraManager mMyCameraManager;
@@ -24,8 +15,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        MainActivityPermissionsDispatcher.initWithPermissionCheck(this);
 
         setContentView(R.layout.activity_main);
 
@@ -51,16 +40,5 @@ public class MainActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         mCircleRendererView.destroy();
-    }
-
-    @NeedsPermission({Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE})
-    public void init() {
-        mContext = this;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 }
